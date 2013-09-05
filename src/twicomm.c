ffde6357d401580ca84be3c418520ec253c4a48f
Thomas Kastner <thomas.kastner@student.tugraz.at>
2013-09-05 08:08:25 +0200

    initial commit

---
+++ src/twicomm.c
@@ -0,0 +1,18 @@
+/*
+ * twicomm.c
+ *
+ *  Created on: Sep 5, 2013
+ *      Author: tkastner
+ */
+
+#include <stdio.h>
+#include <bcm2835.h>;
+
+#define SDA RP_GPIO_P1_03
+
+int main() {
+
+	printf("Hello World\n");
+
+	return 0;
+}
